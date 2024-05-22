518f563499c9e7e1c627f8224c989e0eaf391c92
Edmond Verdier <edmond.verdier@sicara.com>
2024-05-22 11:24:44 +0200

    expert_3

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from src.utils.get_transcription import get_transcription
 from src.utils.get_answer import get_answer
 from src.utils.generate_chunks import generate_chunks
 
+import chromadb
+from chromadb.db.base import UniqueConstraintError
+
 st.header("AI Note Taker 🤖")
 st.write("Welcome to the AI Note Taker! 🎉")
 
@@ -20,19 +23,38 @@ if audio_bytes:
     st.write("Transcription:")
     transcription = get_transcription(AUDIO_PATH)
     st.write(transcription)
+
+    chunks = generate_chunks(transcription)
+    chroma_client = chromadb.Client()
+
+    # Create a collection if it doesn't exist
+    try:
+        collection = chroma_client.create_collection(name="ai_notetaker")
+    except UniqueConstraintError:
+        collection = chroma_client.get_collection(name="ai_notetaker")
+
+    collection.add(documents = chunks,
+                    ids = [str(i) for i in range(len(chunks))])
+    
+    st.write("Chunks:")
+    for i, chunk in enumerate(chunks):
+        st.write(f"{i}: {chunk}")
     
     question = st.text_input(label="Question", placeholder="Enter a question please")
     if question:
         answers = []
-        for chunk in generate_chunks(transcription):
-            message = f"""
-                Context: {chunk},
+        results = collection.query(
+            query_texts=[question], # Chroma will embed this for you
+            n_results=2 # how many results to return
+        )
+        message = ""
+        for result in results:
+            message += f"Context: {results['documents']}"
+        message += f"""
                 Question: {question}
-                If the answer is not in the context above, please answer with empty string "".
+                If the answer is not in the context above, please answer with empty string "Not in the context".
                 Answer:
             """
-            answers.append(get_answer(message))
-        
-        # Join the answers into a single string
-        answer = " ".join(answers)
+
+        answer = get_answer(message)
         st.write(answer)
