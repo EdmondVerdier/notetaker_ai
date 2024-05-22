7bd9f20bce641c94a9fceba3245ec5e470d214ec
Edmond Verdier <edmond.verdier@sicara.com>
2024-05-22 11:11:47 +0200

    pro_5

---
+++ app.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import streamlit as st
+from audio_recorder_streamlit import audio_recorder
+
+from src.utils.get_transcription import get_transcription
+from src.utils.get_answer import get_answer
+from src.utils.generate_chunks import generate_chunks
+
+import chromadb
+
+st.header("AI Note Taker 🤖")
+st.write("Welcome to the AI Note Taker! 🎉")
+
+AUDIO_PATH = Path("audio.wav")
+
+audio_bytes = audio_recorder()
+if audio_bytes:
+    with open(AUDIO_PATH, mode='wb') as f:
+        f.write(audio_bytes)
+    st.audio(audio_bytes, format="audio/wav")
+
+    st.write("Transcription:")
+    transcription = get_transcription(AUDIO_PATH)
+    st.write(transcription)
+    
+    question = st.text_input(label="Question", placeholder="Enter a question please")
+    if question:
+        answers = []
+        for chunk in generate_chunks(transcription):
+            message = f"""
+                Context: {chunk},
+                Question: {question}
+                If the answer is not in the context above, please answer with empty string "".
+                Answer:
+            """
+            answers.append(get_answer(message))
+        
+        # Join the answers into a single string
+        answer = " ".join(answers)
+        st.write(answer)
---
+++ src/utils/get_transcription.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+from openai import OpenAI
+
+from credentials.credentials import OPENAI_API_KEY
+
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+client = OpenAI()
+
+def get_transcription(audio: Path) -> str:
+    return client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio,
+    ).text
